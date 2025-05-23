d97b9ea0d5e7f3e528dae653f60fb2b5e1f4a913
Andrew Sonin <sonin.cel@gmail.com>
2025-05-23 18:19:40 +0300

    feat!(static): remove redundant files; use only minimized `.js` and `.css`

--- static/utils/js/right-quote.js
+++
@@ -1,5 +0,0 @@
-// originally to right-align a quote footer if it starts with ---; now it just
-// right-align all paragraphs that start with em-dashes
-[...document.getElementsByTagName('p')].forEach(p => {
-  if (/^(—|―|---)/.test(p.textContent)) p.style.textAlign = 'right';
-});
